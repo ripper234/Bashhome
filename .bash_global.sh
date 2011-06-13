437e2581364ee09edb62362d8cdd94e736fd90e9
Ron Gross <rgross@google.com>
2011-06-13 16:57:24 +0300

    minor

--- .bash_global.sh
+++ .bash_global.sh
@@ -72,6 +72,7 @@ fi ###Export ???
 # If this is an xterm set the title to user@host:dir
 case "$TERM" in
 xterm*|rxvt*)
+    # Uncomment this to get full path prompt
     # PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
     ;;
 *)
