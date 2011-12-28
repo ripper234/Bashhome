0a8099fa8136eb070fc66bc15f7d98937dc4b43a
Ron Gross <ron.gross@gmail.com>
2011-12-28 17:12:41 +0200

    Color fixes

--- .scripts/bash_global.sh
+++ .scripts/bash_global.sh
@@ -21,12 +21,13 @@ if [ -z "$debian_chroot" ] && [ -r /etc/debian_chroot ]; then
     debian_chroot=$(cat /etc/debian_chroot)
 fi
 
+# If you want the hostname, add \h
+# Simple prompt PS1='\W\$ '
 if [ "$color_prompt" = yes ]; then
-    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
 else
     PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
 fi
-# Simple prompt PS1='\W\$ '
 unset color_prompt
 
 # enable color support of ls and also add handy aliases
